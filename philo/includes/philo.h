b9396a51afee2db0c6276b5cca2afe07384197e6
Alicja Stefanska <alicja.stefanska44@gmail.com>
2024-10-21 12:02:50 +0200

    first

---
+++ philo/includes/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astefans <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 17:39:33 by astefans          #+#    #+#             */
+/*   Updated: 2024/08/09 17:39:44 by astefans         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+
+#endif
+
