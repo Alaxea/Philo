fae65ac49f5547faabb67c5481e70319ae9370a8
Alicja Stefanska <alicja.stefanska44@gmail.com>
2024-11-15 12:15:53 +0100

    test

--- philo/time.c
+++ philo/time.c
@@ -32,7 +32,7 @@ void	ft_usleep(long time)
 void	usage(void)
 {
 	printf("Usage: ./philo [number_of_philosophers] [time_to_die]\
-		[time_to_eat] [time_to_sleep]\
-		[number_of_times_each_philosopher_must_eat]\n");
+[time_to_eat] [time_to_sleep]\
+[number_of_times_each_philosopher_must_eat]\n");
 	exit(1);
 }
